730a5917d307fdcc46f1e2e21436229154db9b1d
Axentra-Nexus <business@axentra.co.in>
2026-05-14 03:29:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    tailwindcss(),
-  ],
+  root: '.',
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist'
+  }
 })
